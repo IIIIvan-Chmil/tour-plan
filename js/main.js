bdb396613438266a4c4df51c05f22e9728de1a9f
IIIIvan-Chmil <chmiliiiivan@gmail.com>
2021-03-09 18:46:22 +0300

    create: button control

--- js/main.js
+++ js/main.js
@@ -7,4 +7,10 @@ const swiper = new Swiper('.swiper-container', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+
+  // Enables navigation through slides using keyboard
+   keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 });
