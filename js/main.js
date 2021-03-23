3ed0da22a911f39c5622633bd01c51479f94382f
IIIIvan-Chmil <chmiliiiivan@gmail.com>
2021-03-23 09:25:46 +0300

    create: модальное на кнопки валидация частично

--- js/main.js
+++ js/main.js
@@ -1,61 +1,80 @@
 $(document).ready(function () {
-  const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+    const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: "coverflow",
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: "coverflow",
 
-  // Enables navigation through slides using keyboard
-   keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    // Enables navigation through slides using keyboard
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
 
-  // Enables navigation through slides using keyboard
-   keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    // Enables navigation through slides using keyboard
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-var menuButton = document.querySelector(".menu-button");
-  menuButton.addEventListener("click", function() {
-  console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
-});
+  var menuButton = document.querySelector(".menu-button");
+    menuButton.addEventListener("click", function() {
+    console.log("Клик по кнопке меню");
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  });
 
-var modalButton = $("[data-toggle=modal]");
-var closeModalButton = $(".modal__close");
-modalButton.on('click', openModal);
-closeModalButton.on("click", closeModal)
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on("click", closeModal)
 
-function openModal() {
-  var modalOverlay = $(".modal__overlay");
-  var modalDialog = $(".modal__dialog");
-  modalOverlay.addClass('modal__overlay--visible');
-  modalDialog.addClass('modal__dialog--visible');
-}
-function closeModal(event) {
-  event.preventDefault();
-  var modalOverlay = $(".modal__overlay");
-  var modalDialog = $(".modal__dialog");
-  modalOverlay.removeClass('modal__overlay--visible');
-  modalDialog.removeClass('modal__dialog--visible');
-}
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+  // Обработка форм
+  $('.form').each(function () {
+      $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlenght: "Имя должно быть не короче 2 букв",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Телефон обязателен",
+        },
+      },
+    });
+  });
 });
